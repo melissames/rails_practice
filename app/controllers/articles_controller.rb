4a07dd2e9d538db46b8ec9e7fa6dfbf483175057
Barkbox Loaner <barkboxloaner@Barkboxs-MacBook-Air.local>
2018-09-17 17:18:26 -0400

    added show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,16 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     # this shows the params in json
     @article = Article.new(article_params)
-    @article.save
-    redirect_to: articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully saved"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
